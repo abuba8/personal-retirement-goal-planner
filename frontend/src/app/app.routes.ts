d4769e5bf4fbcc5083fa9537113e85608a7ebbea
Abdullah Ahmad <40513745+abuba8@users.noreply.github.com>
2026-07-13 17:57:52 -0400

    added functionality for logInGuard

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,18 +1,18 @@
 import { Routes } from '@angular/router';
-import { authGuard } from './core/auth-guard';
+import { authGuard, loggedInGuard } from './core/auth-guard';
 
 export const routes: Routes = [
     {
         path: '', redirectTo: 'login', pathMatch: 'full'
     },
     {
-        path: 'login', loadComponent: () => import('./pages/login/login').then((x) => x.Login),
+        path: 'login', canActivate: [loggedInGuard], loadComponent: () => import('./pages/login/login').then((x) => x.Login),
     },
     {
-        path: 'register', loadComponent: () => import('./pages/register/register').then((x) => x.Register),
+        path: 'register', canActivate: [loggedInGuard], loadComponent: () => import('./pages/register/register').then((x) => x.Register),
     },
     {
-        path: 'verify', loadComponent: () => import('./pages/verify/verify').then((x) => x.Verify),
+        path: 'verify', canActivate: [loggedInGuard], loadComponent: () => import('./pages/verify/verify').then((x) => x.Verify),
     },
     {
         path: 'goals', canActivate: [authGuard], loadComponent: () => import('./pages/goals/goals').then((x) => x.Goals),
