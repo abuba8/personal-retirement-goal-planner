71089e908f15d46d872372237cbf3050d92f2741
Abdullah Ahmad <40513745+abuba8@users.noreply.github.com>
2026-07-14 20:36:34 -0400

    previous commit, new branch

--- frontend/src/environments/environments.ts
+++ frontend/src/environments/environments.ts
@@ -1,6 +1,5 @@
 // src/environments/environments.ts
 export const environment = {
   production: false,
-  // change the port to match server.port (your .env PORT) if it isn't 8080
   baseApiUrl: "http://localhost:8081",
 };
